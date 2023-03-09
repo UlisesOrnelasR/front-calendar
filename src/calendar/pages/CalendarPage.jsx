5e7591b0af881f65c1af633b72591739b1d97b0a
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2023-03-09 09:13:50 -0700

    chore: Add react-big-calendar

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,10 +1,47 @@
-import React from "react";
+import { Calendar, dateFnsLocalizer } from "react-big-calendar";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import enUS from "date-fns/locale/en-US";
+
 import { Navbar } from "../";
+import { addHours, format, parse, startOfWeek, getDay } from "date-fns";
+
+const locales = {
+  "en-US": enUS,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
+
+const events = [
+  {
+    title: "All Day Event very long title",
+    notes: "Some notes about this event",
+    start: new Date(),
+    end: addHours(new Date(), 2),
+    bgcolor: "#fafafa",
+    user: {
+      _id: "123",
+      name: "Ulises",
+    },
+  },
+];
 
 export const CalendarPage = () => {
   return (
     <>
       <Navbar />
+      <Calendar
+        localizer={localizer}
+        events={events}
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: "calc(100vh - 80px)" }}
+      />
     </>
   );
 };
