890c42346f07f95912bf9e2a72c337797b314ad7
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2023-04-11 16:19:45 -0700

    chore: Creating new event in calendar

--- src/hooks/useCalendarStore.js
+++ src/hooks/useCalendarStore.js
@@ -1,4 +1,5 @@
 import { useDispatch, useSelector } from "react-redux";
+import { calendarApi } from "../api";
 import {
   onSetActiveEvent,
   onAddNewEvent,
@@ -10,24 +11,26 @@ export const useCalendarStore = () => {
   const dispatch = useDispatch();
 
   const { events, activeEvent } = useSelector((state) => state.calendar);
+  const { user } = useSelector((state) => state.auth);
 
   const setActiveEvent = (calendarEvent) => {
     dispatch(onSetActiveEvent(calendarEvent));
   };
 
   const startSavingEvent = async (calendarEvent) => {
-    // TODO: llegar al backend
-
     // Todo bien
     if (calendarEvent._id) {
       //actualizando un evento existente
       dispatch(onUpdateEvent({ ...calendarEvent }));
     } else {
       //creando un nuevo evento
+      const { data } = await calendarApi.post("/events", calendarEvent);
+      console.log(data);
       dispatch(
         onAddNewEvent({
           ...calendarEvent,
-          _id: new Date().getTime(),
+          id: data.event.id,
+          user,
         })
       );
     }
