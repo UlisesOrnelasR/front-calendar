0fdc5d4a5dfcec98c9c42baa2c4a6bffb7a2e45d
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2023-04-13 16:56:28 -0700

    test:  openDateModal change to true isDateModalOpen

--- tests/hooks/useUiStore.test.js
+++ tests/hooks/useUiStore.test.js
@@ -1,4 +1,4 @@
-import { renderHook } from "@testing-library/react";
+import { act, renderHook } from "@testing-library/react";
 import { useUiStore } from "../../src/hooks/useUiStore";
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
@@ -32,5 +32,25 @@ describe("Pruebas en useUiStore", () => {
       closeDateModal: expect.any(Function),
       toggleDateModal: expect.any(Function),
     });
+  });
+
+  test("openDateModal debe de colocar true en el isDateModalOpen", () => {
+    const mockStore = getMockStore({
+      isDateModalOpen: false,
+    });
+    const { result } = renderHook(() => useUiStore(), {
+      wrapper: ({ children }) => (
+        <Provider store={mockStore}>{children}</Provider>
+      ),
+    });
+    // console.log(result.current);
+
+    const { openDateModal } = result.current;
+
+    act(() => {
+      openDateModal();
+    });
+    // console.log(result.current);
+    expect(result.current.isDateModalOpen).toBeTruthy();
   });
 });
